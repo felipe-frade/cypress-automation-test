ddbeedae9b7f1699c36f6c7cebcbef148cf57c5c
Felipe Frade de Oliveira Pereira <39383884+felipe-frade@users.noreply.github.com>
2025-02-15 21:37:32 -0300

    init project

---
+++ cypress/constants/web-elements.ts
@@ -0,0 +1,10 @@
+export const webElements = {
+    pages: {
+        home: {
+            title: 'h1.page-title',
+            main: 'a.home-main',
+            productItems: 'ol.product-items',
+            productItem: 'li.product-item',
+        }
+    }
+}
---
+++ cypress/e2e/home-page.cy.ts
@@ -0,0 +1,15 @@
+import { webElements } from "../constants/web-elements";
+
+describe('Home Page', () => {
+    beforeEach(() => {
+        // https://magento.softwaretestingboard.com - see cypress.env.json or cypress.env-example.json
+        cy.visit(`${Cypress.env('baseUrl')}`);
+    });  
+    it('Should open', () => {     
+        cy.get(webElements.pages.home.title, { timeout: 3000 }).should('be.visible');
+        cy.get(webElements.pages.home.main, { timeout: 3000 }).should('be.visible');
+        cy.get(webElements.pages.home.productItems, { timeout: 3000 }).should('be.visible');
+        cy.get(webElements.pages.home.productItems, { timeout: 3000 })
+            .find(webElements.pages.home.productItem).should('have.length.greaterThan', 3);
+    }); 
+});
